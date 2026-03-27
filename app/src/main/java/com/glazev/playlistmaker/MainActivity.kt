6c3a354f8c9bb04f6ee41d171adf73a09ca21a5f
Glazev-Artem <glazev_artem@inbox.ru>
2026-03-27 18:09:32 +0700

    ДЗ по 8 спринту

--- app/src/main/java/com/glazev/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/glazev/playlistmaker/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.glazev.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonLibrary = findViewById<Button>(R.id.button_library)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        // Способ 1: Использование анонимного класса для перехода на экран Поиска
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        buttonSearch.setOnClickListener(searchClickListener)
+
+        // Способ 2: Использование лямбда-выражения для перехода в Медиатеку
+        buttonLibrary.setOnClickListener {
+            val intent = Intent(this, LibraryActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Переход в Настройки
+        buttonSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
