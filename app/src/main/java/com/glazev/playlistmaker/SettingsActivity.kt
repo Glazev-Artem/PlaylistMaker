53c4b603f1ced6f937806a2132f035689999cd63
Glazev-Artem <glazev_artem@inbox.ru>
2026-04-10 13:56:29 +0700

    сдача дз по спринту №9

--- app/src/main/java/com/glazev/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/glazev/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,13 @@
 package com.glazev.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +15,47 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageView>(R.id.back_button)
-
         backButton.setOnClickListener {
             finish()
+        }
+
+        val themeSwitch = findViewById<SwitchMaterial>(R.id.theme_switch)
+
+        // Устанавливаем текущее состояние переключателя в зависимости от текущей темы
+        themeSwitch.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        val shareButton = findViewById<FrameLayout>(R.id.share_button)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_link))
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.support_button)
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_body))
+            }
+            startActivity(supportIntent)
+        }
+
+        val agreementButton = findViewById<FrameLayout>(R.id.agreement_button)
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_link)))
+            startActivity(agreementIntent)
         }
     }
 }
